7de92e8d069623bd62c22a8821a3302323f681fd
Inocent OLORY-TOGBE <Inocent.OLORY@chem.loc>
2025-04-22 15:17:35 +0100

    Integration de la vue evaluateur en cours

---
+++ tracking-annotation/src/views/evaluation.vue
@@ -0,0 +1,15 @@
+<script setup>
+import RecordAdioTrack from '@/components/recordAdioTrack.vue';
+
+
+</script>
+
+<template>
+    <div>
+        <RecordAdioTrack/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
